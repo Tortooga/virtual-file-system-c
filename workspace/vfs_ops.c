447c64e37c70f1f78ffb966fca6f38f1f42090ad
Omer Almahi <oazeem37@gmail.com>
2026-05-22 12:20:47 +0300

    added vfs_ops function ws_create_folder

--- workspace/vfs_ops.c
+++ workspace/vfs_ops.c
@@ -21,3 +21,22 @@ StatusCode ws_create_file(Workspace *workspace, char *file_name)
         workspace->cur_folder
     );
 }
+
+//folder_name must be null terminated
+StatusCode ws_create_folder(Workspace *workspace, char *folder_name)
+{
+    if (!workspace || !folder_name)
+    {
+        return NULL_POINTER_PASSED;
+    }
+
+    //vfs_sub_folder_init validates folder_name
+    return vfs_sub_folder_init(
+        NULL,
+        workspace->entry_store,
+        folder_name,
+        strlen(folder_name),
+        workspace->cur_folder
+    );
+}
+
--- workspace/vfs_ops.h
+++ workspace/vfs_ops.h
@@ -1,5 +1,13 @@
+#ifndef VFS_OPS_H
+#define VFS_OPS_H
+
 #include "status.h"
 #include "workspace.h"
 
 //file_name must be null terminated
 StatusCode ws_create_file(Workspace *workspace, char *file_name);
+
+//folder_name must be null terminated
+StatusCode ws_create_folder(Workspace *workspace, char *folder_name);
+
+#endif
